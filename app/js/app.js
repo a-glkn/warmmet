4bcc40f657d0c7823f66934cd8d95277cfc66bec
Alex <galkinamaria37@gmail.com>
2023-05-22 19:46:41 +0700

    soc-block edit; showroom block

--- app/js/app.js
+++ app/js/app.js
@@ -13,21 +13,42 @@ document.addEventListener('DOMContentLoaded', () => {
     var im = new Inputmask("+7 (999) 999 99 99");
     im.mask(selector);
 
-    new Swiper(".main-slider", {
-        loop: true,
-        effect: 'fade',
-        autoplay: {
-            delay: 5000,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: '.swiper-pagination'
-        },
-    });
+    let mainSlider = document.querySelectorAll('.main-slider');
 
+    if(mainSlider) {
+        new Swiper(".main-slider", {
+            loop: true,
+            effect: 'fade',
+            autoplay: {
+                delay: 5000,
+            },
+            navigation: {
+                nextEl: ".main-slider .swiper-button-next",
+                prevEl: ".main-slider .swiper-button-prev",
+            },
+            pagination: {
+                el: '.main-slider .swiper-pagination'
+            },
+        });
+    }
+
+
+    let socSlider = document.querySelectorAll('.soc-slider');
+
+    if(socSlider) {
+        new Swiper(".soc-slider", {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            autoplay: {
+                delay: 5000,
+            },
+            navigation: {
+                nextEl: ".soc-slider .swiper-button-next",
+                prevEl: ".soc-slider .swiper-button-prev",
+            }
+        });
+    }
+  
     var togglers = document.querySelectorAll(".menu-toggler");
 	togglers.forEach((t) => {
         t.addEventListener("click",function(e){
